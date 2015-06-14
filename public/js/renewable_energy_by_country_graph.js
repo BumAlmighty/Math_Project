847595afa8a88c358ab52cbea961d72d8bd3b6b5
reki <rekihattori@gmail.com>
2015-06-13 21:27:31 -0400

    adds dynamic data to graph

--- public/js/renewable_energy_by_country_graph.js
+++ public/js/renewable_energy_by_country_graph.js
@@ -6,82 +6,70 @@ Papa.parse('/data_sets/csv/enigma-gov.eu.eurostats.tb-eu.sd.sd-cc.sd-cc-nrg.tsdc
     complete: function(results) {
       console.log(results);
 
+      var series_data = [];
+
+      $( results.data ).each(function( key, value ) {
+        if (key != 0) {
+          var value3 =  isNaN(value[3]) ? 3 : parseInt(value[3]);
+          var value5 =  isNaN(value[5]) ? 5 : parseInt(value[5]);
+          var value7 =  isNaN(value[7]) ? 7 : parseInt(value[7]);
+          var value9 =  isNaN(value[9]) ? 9 : parseInt(value[9]);
+          var value11 = isNaN(value[11]) ? 11 : parseInt(value[11]);
+          var value13 = isNaN(value[13]) ? 13 : parseInt(value[13]);
+          var value15 = isNaN(value[15]) ? 15 : parseInt(value[15]);
+          var value17 = isNaN(value[17]) ? 17 : parseInt(value[17]);
+          var value19 = isNaN(value[19]) ? 19 : parseInt(value[19]);
+
+          series_data.push(
+            {
+              name: value[2],
+              data: [value3, value5, value7, value9, value11, value13, value15, value17, value19]
+            }
+          );
+        }
+      })
+
       $(function () {
-        $('#container2').highcharts({
+        $('#renewable_energy_container').highcharts({
           chart: {
             type: 'area'
           },
           title: {
-            text: 'Share of renewable energy by Country'
+            text: 'Historic and Estimated Worldwide Population Distribution by Region'
           },
           subtitle: {
-            text: 'Source: <a href="http://thebulletin.metapress.com/content/c4120650912x74k7/fulltext.pdf">' +
-              'thebulletin.metapress.com</a>'
+            text: 'Source: Wikipedia.org'
           },
           xAxis: {
-            allowDecimals: false,
-            labels: {
-              formatter: function () {
-                return this.value; // clean, unformatted number for year
-              }
+            categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+            tickmarkPlacement: 'on',
+            title: {
+              enabled: false
             }
           },
           yAxis: {
             title: {
-              text: 'Nuclear weapon states'
-            },
-            labels: {
-              formatter: function () {
-                return this.value / 1000 + 'k';
-              }
+              text: 'Percent'
             }
           },
           tooltip: {
-            pointFormat: '{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.percentage:.1f}%</b> ({point.y:,.0f} millions)<br/>',
+            shared: true
           },
           plotOptions: {
             area: {
-              pointStart: 1940,
+              stacking: 'percent',
+              lineColor: '#ffffff',
+              lineWidth: 1,
               marker: {
-                enabled: false,
-                symbol: 'circle',
-                radius: 2,
-                states: {
-                  hover: {
-                    enabled: true
-                  }
-                }
+                lineWidth: 1,
+                lineColor: '#ffffff'
               }
             }
           },
-          series: [
-            $( results.data ).each(function( key, value ) {
-              if (key != 0) {
-              }
-            });
-
-            {
-              name: 'USA',
-              data: [null, null, null, null, null, 6, 11, 32, 110, 235, 369, 640,
-                1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126,
-                27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662,
-                26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
-                24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586,
-                22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950,
-              10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104]
-            }, {
-              name: 'USSR/Russia',
-              data: [null, null, null, null, null, null, null, null, null, null,
-                5, 25, 50, 120, 150, 200, 426, 660, 869, 1060, 1605, 2471, 3322,
-                4238, 5221, 6129, 7089, 8339, 9399, 10538, 11643, 13092, 14478,
-                15915, 17385, 19055, 21205, 23044, 25393, 27935, 30062, 32049,
-                33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000, 37000,
-                35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
-              21000, 20000, 19000, 18000, 18000, 17000, 16000]
-            }]
+          series: series_data
         });
       });
-
     }
 })
 
